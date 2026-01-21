67803d5ab3b0a6c25caa369cdeb7ec95fd5c75d2
Faizan <mdfaizanlog.in@gmail.com>
2026-01-21 11:23:59 +0530

    chainged the folder name

--- utils.py
+++
@@ -1,9 +0,0 @@
-import base64
-import cv2
-import numpy as np
-
-def base64_to_image(base64_str: str):
-    img_bytes = base64.b64decode(base64_str)
-    img_array = np.frombuffer(img_bytes, np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    return img
